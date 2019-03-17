c4bf0f9227b940efb622b5346dffa717020b8f70
pradeep.mascarenhas <paddum19@gmail.com>
2019-03-17 22:14:48 +0530

    finalone

--- tester/src/main/java/com/ownproj/invoice/container/ConnectionParam.java
+++ tester/src/main/java/com/ownproj/invoice/container/ConnectionParam.java
@@ -2,11 +2,29 @@ package com.ownproj.invoice.container;
 
 public class ConnectionParam {
 
-	public static final String DRIVER="com.mysql.cj.jdbc.Driver";  
+	/*public static final String DRIVER="com.mysql.cj.jdbc.Driver";  
 	public static final String URL="jdbc:mysql://localhost:3306/invoice_alpha?useSSL=false";
 	public static final String USERNAME="root";
-	public static final String PASSWORD="root";
-	
+	public static final String PASSWORD="root";*/
+
 	//Pdf File Upload Path
 	public static final String PDF_UPLOAD_PATH="C:\\Users\\pradeep.mascarenhas\\Desktop\\GroupAmountCalculator\\PdfFolder";
+
+
+	public static final String DRIVER="com.mysql.cj.jdbc.Driver";  
+	public static final String URL="jdbc:mysql://mysql:3306/invoicedb?useSSL=false";
+	public static final String USERNAME="userF2K";
+	public static final String PASSWORD="KYkykFBldCBH05Tt";
+	
+	/*
+	 Openshift
+	  Username: userF2K
+	  Password: KYkykFBldCBH05Tt
+	  Database Name: invoicedb
+	 Connection URL: mysql://mysql:3306/
+
+	For more information about using this template, including OpenShift considerations, 
+	see https://github.com/sclorg/mysql-container/blob/master/5.7/root/usr/share/container-scripts/mysql/README.md .
+	 */
+	
 }
